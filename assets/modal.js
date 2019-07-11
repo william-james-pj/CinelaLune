3d6ed3bdaa2d6954e3b17c8f0f2a09f712537fc1
william-jameS2 <52788612+william-jameS2@users.noreply.github.com>
2019-07-11 12:42:49 -0300

    Add files via upload

---
+++ assets/modal.js
@@ -0,0 +1,124 @@
+
+// Modal 1
+var t1 = document.getElementById('t1');
+var d1 = document.getElementById('d1');
+var dia1 = document.getElementById('dia1');
+var hora1 = document.getElementById('hora1');
+var e1 = document.getElementById('e1');
+var c1 = document.getElementById('c1');
+var b1 = document.getElementById('b1');
+var r1 = document.getElementById('r1');
+var end1 = document.getElementById('end1');
+
+var titulo1 = "Vingadores: Ultimato";
+var descricao1 = "Depois dos devastadores eventos de Vingadores: Guerra Infinita, o Universo está em ruínas por conta dos esforços do Titã Louco, Thanos. Com a ajuda dos seus aliados restantes, os Vingadores devem se reunir mais uma vez para desfazer as ações de Thanos e restaurar a ordem do universo de uma vez por todas, não importam as consequências que os esperam. Classificação indicativa 12 anos, contém violência e drogas lícitas.";
+var data1 = "07/07/2019";
+var horas1 = "18:50";
+var estado1 = "São Paulo";
+var cidade1 = "São Roque";
+var bairro1 = "Centro";
+var rua1 = "Rua n sei";
+var endereco1 = "Praça da Matriz";
+
+t1.innerHTML = titulo1;
+d1.innerHTML = descricao1;
+dia1.innerHTML = data1;
+hora1.innerHTML = horas1;
+e1.innerHTML = estado1;
+c1.innerHTML = cidade1;
+b1.innerHTML = bairro1;
+r1.innerHTML = rua1;
+end1.innerHTML = endereco1;
+
+// Modal 2
+var t2 = document.getElementById('t2');
+var d2 = document.getElementById('d2');
+var dia2 = document.getElementById('dia2');
+var hora2 = document.getElementById('hora2');
+var e2 = document.getElementById('e2');
+var c2 = document.getElementById('c2');
+var b2 = document.getElementById('b2');
+var r2 = document.getElementById('r2');
+var end2 = document.getElementById('end2');
+
+var titulo2 = "Annabelle 3: De Volta Para Casa";
+var descricao2 = "Quando Ed (Patrick Wilson) e Lorraine Warren (Vera Farmiga) deixam sua casa durante um fim de semana para investigar mais um caso paranormal, a filha do casal, a pequena Judy Warren (McKenna Grace), é deixada aos cuidados de sua babá (Madison Iseman). Mas as duas entram em perigo quando uma amiga delas invade a Sala dos Artefatos dos Warren e abre a cristaleira onde a maligna boneca Annabelle estava contida devolvendo à boneca o poder de animar os letais e aterrorizantes objetos contidos na Sala. Classificação indicativa 14 anos, contém medo e violência.";
+var data2 = "14/07/2029";
+var horas2 = "18:50";
+var estado2 = "São Paulo";
+var cidade2 = "São Roque";
+var bairro2 = "Centro";
+var rua2 = "Rua n sei";
+var endereco2 = "Praça da Matriz";
+
+t2.innerHTML = titulo2;
+d2.innerHTML = descricao2;
+dia2.innerHTML = data2;
+hora2.innerHTML = horas2;
+e2.innerHTML = estado2;
+c2.innerHTML = cidade2;
+b2.innerHTML = bairro2;
+r2.innerHTML = rua2;
+end2.innerHTML = endereco2;
+
+// Modal 3
+var t3 = document.getElementById('t3');
+var d3 = document.getElementById('d3');
+var dia3 = document.getElementById('dia3');
+var hora3 = document.getElementById('hora3');
+var e3 = document.getElementById('e3');
+var c3 = document.getElementById('c3');
+var b3 = document.getElementById('b3');
+var r3 = document.getElementById('r3');
+var end3 = document.getElementById('end3');
+
+var titulo3 = "Homem-Aranha - Longe de Casa";
+var descricao3 = "Peter Parker (Tom Holland) está em uma viagem de duas semanas pela Europa, ao lado de seus amigos de colégio, quando é surpreendido pela visita de Nick Fury (Samuel L. Jackson). Convocado para mais uma missão heróica, ele precisa enfrentar vários vilões que surgem em cidades-símbolo do continente, como Londres, Paris e Veneza, e também a aparição do enigmático Mysterio (Jake Gyllenhaal). Classificação indicativa 30 anos, contém violência.";
+var data3 = "21/07/2039";
+var horas3 = "18:50";
+var estado3 = "São Paulo";
+var cidade3 = "São Roque";
+var bairro3 = "Centro";
+var rua3 = "Rua n sei";
+var endereco3 = "Praça da Matriz";
+
+t3.innerHTML = titulo3;
+d3.innerHTML = descricao3;
+dia3.innerHTML = data3;
+hora3.innerHTML = horas3;
+e3.innerHTML = estado3;
+c3.innerHTML = cidade3;
+b3.innerHTML = bairro3;
+r3.innerHTML = rua3;
+end3.innerHTML = endereco3;
+
+// Modal 4
+var t4 = document.getElementById('t4');
+var d4 = document.getElementById('d4');
+var dia4 = document.getElementById('dia4');
+var hora4 = document.getElementById('hora4');
+var e4 = document.getElementById('e4');
+var c4 = document.getElementById('c4');
+var b4 = document.getElementById('b4');
+var r4 = document.getElementById('r4');
+var end4 = document.getElementById('end4');
+
+var titulo4 = "O Rei Leão";
+var descricao4 = "Simba (Donald Glover) é um jovem leão cujo destino é se tornar o rei da selva. Entretanto, uma armadilha elaborada por seu tio Scar (Chiwetel Ejiofor) faz com que Mufasa (James Earl Jones), o atual rei, morra ao tentar salvar o filhote. Consumido pela culpa, Simba deixa o reino rumo a um local distante, onde encontra amigos que o ensinam a mais uma vez ter prazer pela vida. Classificação indicativa 10 anos, contém violência.";
+var data4 = "28/07/2049";
+var horas4 = "18:50";
+var estado4 = "São Paulo";
+var cidade4 = "São Roque";
+var bairro4 = "Centro";
+var rua4 = "Rua n sei";
+var endereco4 = "Praça da Matriz";
+
+t4.innerHTML = titulo4;
+d4.innerHTML = descricao4;
+dia4.innerHTML = data4;
+hora4.innerHTML = horas4;
+e4.innerHTML = estado4;
+c4.innerHTML = cidade4;
+b4.innerHTML = bairro4;
+r4.innerHTML = rua4;
+end4.innerHTML = endereco4;
