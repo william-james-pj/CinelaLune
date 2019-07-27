8e0491d1d0c1881916658ebca78032704b338024
william-jameS2 <52788612+william-jameS2@users.noreply.github.com>
2019-07-27 12:20:49 -0300

    Add files via upload

--- js/codigos.js
+++ js/codigos.js
@@ -35,4 +35,12 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+
+  $('.programacao-modal').on('hide.bs.modal', function(e) {    
+    var $if = $(e.delegateTarget).find('iframe');
+    var src = $if.attr("src");
+    $if.attr("src", '/empty.html');
+    $if.attr("src", src);
+  });
+
 })(jQuery); // End of use strict
